42f3c57895a0369cb40ea7160977e72b1fe8cd8d
goarum <86760528+goarum@users.noreply.github.com>
2021-09-07 18:30:47 +0900

    Add files via upload

---
+++ pay/src/main/java/yanolja/PolicyHandler.java
@@ -0,0 +1,78 @@
+package yanolja;
+
+import yanolja.config.kafka.KafkaProcessor;
+
+import java.util.Optional;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+    @Autowired PaymentRepository paymentRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverCancelReserved_Refund(@Payload CancelReserved cancelReserved){
+
+
+        try {
+            if (!cancelReserved.validate()) return;
+
+            System.out.println("\n\n##### listener ReserveCheck : " + cancelReserved.toJson() + "\n\n");
+                
+            Optional<Payment> paymentOptional = paymentRepository.findById(cancelReserved.getId());
+
+            if( paymentOptional.isPresent()) {
+                 Payment payment = paymentOptional.get();
+            
+                 payment.setId(cancelReserved.getId());
+
+                 System.out.println("**** Refund "+ payment.getPrice() + "원 and reservation Room " + payment.getId() + " is Canceled ****" );
+                
+                 paymentRepository.save(payment);
+                }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+            
+    }
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverRoomOffered_SendToRoom(@Payload RoomOffered roomOffered){
+
+        try {
+            if (!roomOffered.validate()) return;
+
+            System.out.println("\n\n##### listener ReserveCheck : " + roomOffered.toJson() + "\n\n");
+                
+            Optional<Payment> paymentOptional = paymentRepository.findById(roomOffered.getId());
+
+            if( paymentOptional.isPresent()) {
+                 Payment payment = paymentOptional.get();
+            
+                 payment.setId(roomOffered.getId());
+
+                 System.out.println("**** Send To Seller "+ payment.getPrice() + "원  to Room " + payment.getId() + " ****" );
+                
+                 paymentRepository.save(payment);
+                }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whatever(@Payload String eventString){}
+
+
+}
